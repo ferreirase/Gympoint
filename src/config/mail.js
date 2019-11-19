86ed9a43131ea29c58507346b5cbfd0e79558521
Ferreirase <42661796+ferreirase@users.noreply.github.com>
2019-11-18 21:49:59 -0300

    Update mail.js

--- src/config/mail.js
+++ src/config/mail.js
@@ -3,8 +3,8 @@ export default {
   port: 2525, //porta
   secure: false, //se usa SSL ou não
   auth:{
-    user: '7632303b7c1e68', //usuário, geralmente email
-    pass: '4468a463c3fc86' //senha
+    user: '***********', //usuário, geralmente email
+    pass: '************' //senha
   }, 
   default: { //configurações padrão para todo envio de email
     from: 'Equipe Gympoint <noreply@gympoint.com>' //remetente padrão
